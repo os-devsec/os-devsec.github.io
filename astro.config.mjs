012826505363b21294688654d3fa5e3d4cbf4519
Oscar <oscaral.lara@gmail.com>
2025-12-22 19:06:41 -0500

    fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import edgeone from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
   
   site: 'https://os-devsec.github.io',
-  base: '/os-devsec/',
-  adapter: edgeone(),
   integrations: [tailwind()],
   server: {
     port: 4321,
